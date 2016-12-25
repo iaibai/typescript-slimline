91a63b8a7c6fc35febf2a723149ea6aa86945d4a
Iain Baird <iain.baird@gmail.com>
2016-12-25 12:14:16 +0000

    First commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# typescript-slimline
+A tiny wee typescript app written to help me understand how the typescript build process works.
---
+++ src/app.ts
@@ -0,0 +1,7 @@
+import * as animals from './animals.ts'
+
+let ho = new animals.Horse('Joe');
+ho.move(15);
+
+let sam = new animals.Snake('Sammy');
+sam.move(2);
